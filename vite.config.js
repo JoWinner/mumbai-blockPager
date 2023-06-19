5a2423fe76a96075da6dd5a629e5df8afba0ecef
JoWinner <jowinner222@gmail.com>
2023-06-19 08:51:25 +0800

    Revert Optimize

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-import { dependencies } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
