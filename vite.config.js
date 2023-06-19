0a556584bd90ed570feb867d597929b75caa984f
JoWinner <jowinner222@gmail.com>
2023-06-19 08:49:16 +0800

    Revert Optimize

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,6 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 import { dependencies } from "./package.json";
 
-// Packages we want in the vendor aka the deps needed in the entire app.
-const globalVendorPackages = ["react", "react-dom", "react-router-dom"];
-
-function renderChunks(deps) {
-  let chunks = {};
-  Object.keys(deps).forEach((key) => {
-    if (globalVendorPackages.includes(key)) return;
-    chunks[key] = [key];
-  });
-  return chunks;
-}
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
@@ -25,15 +12,6 @@ export default defineConfig({
   },
   build: {
     target: "esnext", //browsers can handle the latest ES features
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: globalVendorPackages,
-          ...renderChunks(dependencies),
-        },
-      },
-    },
   },
   resolve: {
     alias: {
